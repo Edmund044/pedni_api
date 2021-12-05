9e9061ac65d171cfd9aa689176f2fa5460827d97
edmund044 <edmundopiyo@gmail.com>
2021-12-05 17:09:08 +0000

    send Email Admin

---
+++ api/routes/admin/sendEmail/sendEmail.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+var nodemailer = require('nodemailer');
+require('dotenv').config();
+const admin = require('../../../../firebase/database');
+const db = admin.firestore();
+
+
+app.post("/orders",async (req,res,next) =>{
+    const data = req.body;
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'edmundopiyo@gmail.com',
+          pass: 'ehmcevmbqwryapyh'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'edmundopiyo@gmail.com',
+        to: req.body.data.reciepient,
+        subject: req.body.data.subject,
+        text: req.body.data.text
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+
+});
