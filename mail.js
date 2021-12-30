1d43494d1b3167d602b20011959e6f120afcebdb
delvaskob <skobdelva@gmail.com>
2021-12-30 22:36:56 +0000

    first commit

--- mail.js
+++ mail.js
@@ -10,7 +10,7 @@ var transporter = nodemailer.createTransport({
 
 var mailOptions = {
   from: 'edmundopiyo@gmail.com',
-  to: 'skobdelva@gmail.com',
+  to: 'marvindesmond@gmail.com',
   subject: 'Sending Email using Node.js Trial 1',
   html: '<h1>Just trying out the Node.js email functionality.</h1>'
 };
